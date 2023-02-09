0ab0374939c35a4062967ad0bc008cdef565df59
karimou cisse <karimou@pylote.io>
2023-02-09 11:47:38 +0100

    [ADD] CommonTheme.js, [CHANGE] AppThemeProvider.js

--- src/themes/AppThemeProvider.js
+++ src/themes/AppThemeProvider.js
@@ -1,75 +1,73 @@
 import { grey, pink } from "@mui/material/colors";
-import { createTheme, ThemeProvider } from "@mui/material/styles" 
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { Component } from "react";
 
 const theme = createTheme({
-    typography: {
-        allVariants: {
-            textTransform: "none",
-            fontSize: 15,
-        },
+  typography: {
+    allVariants: {
+      textTransform: "none",
+      fontSize: 15,
     },
-    palette: {
-        primary: {
-            main: grey[700],
-        },
-        secondary: {
-            main: pink[500],
-        },
+  },
+  palette: {
+    primary: {
+      main: grey[700],
     },
-    Components: {
-        MuiTypography: {
-            defaultProps: {
-                sx: {
-                    px: 1
-                },
-                variant: 'subtitle2',
-                textTransform: 'capitalize'
-            }
+    secondary: {
+      main: pink[500],
+    },
+  },
+  Components: {
+    MuiTypography: {
+      defaultProps: {
+        sx: {
+          px: 1,
         },
-        MuiStack: {
-            defaultProps: {
-                sx: {
-                    px: 2,
-                    py: 1,
-                },
-                spacing: 2,
-                direction: 'row'
-            }
+        variant: "subtitle2",
+        textTransform: "capitalize",
+      },
+    },
+    MuiStack: {
+      defaultProps: {
+        sx: {
+          px: 2,
+          py: 1,
         },
-        MuiPaper: {
-            defaultProps: {
-                elevation: 2,
-            }
+        spacing: 2,
+        direction: "row",
+      },
+    },
+    MuiPaper: {
+      defaultProps: {
+        elevation: 2,
+      },
+    },
+    MuiLink: {
+      defaultProps: {
+        sx: {
+          color: (theme) => theme.palette.primary.main,
         },
-        MuiLink: {
-            defaultProps: {
-                sx: {
-                    color: theme => theme.palette.primary.main
-                },
-                underline: "none"
-            }
-        },
-        MuiButton: {
-            defaultProps: {
-                size: 'small',
-                p: 0,
-                disableRipple: true,
-            },
-            variant: 'text',
-        },
-        MuiTab: {
-            defaultProps: {
-                disableRipple: true,
-            }
-        }
-    }
+        underline: "none",
+      },
+    },
+    MuiButton: {
+      defaultProps: {
+        size: "small",
+        p: 0,
+        disableRipple: true,
+      },
+      variant: "text",
+    },
+    MuiTab: {
+      defaultProps: {
+        disableRipple: true,
+      },
+    },
+  },
 });
 
 const AppThemeProvider = (prop) => {
-  return (
-    <ThemeProvider theme= {theme}>{prop.children}</ThemeProvider>
-  )
-}
+  return <ThemeProvider theme={theme}>{prop.children}</ThemeProvider>;
+};
 
-export default AppThemeProvider
+export default AppThemeProvider;
